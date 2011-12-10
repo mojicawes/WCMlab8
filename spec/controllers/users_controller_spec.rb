85c9eaf8eaa31e8629e5bfcbf32741522de7dd13
Your Name <zaa930@my.utsa.edu>
2011-12-10 15:09:36 -0600

    Added password fields to sign up form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,16 @@ describe UsersController do
   render_views
 
   describe "GET 'new'" do
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content => "Sign up")
+    end
+
     it "returns http success" do
       get :new
       response.should be_success
